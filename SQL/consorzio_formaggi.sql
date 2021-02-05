a7bb892f0ac6df8df6e29669825ea92c6d7642d9
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-02-05 16:34:39 +0100

    Aggiunti gli esercizi del tema del consorzio dei formaggi

---
+++ SQL/consorzio_formaggi.sql
@@ -0,0 +1,58 @@
+SELECT caseficio.name, SUM(forme_vendute) AS numero_forme
+FROM caseificio, attivita
+WHERE caseificio.codice = attivita.codice_caseificio
+AND data <= :data_fine
+AND data >= :data_inizio
+GROUP BY caseificio.codice
+
+SELECT provincia.nome, AVG(attivita.lattelavorato) AS media
+FROM attivita, caseificio, provincia
+WHERE caseificio.codice = attivita.caseificio_codice
+AND caseificio.codice_provincia = provincia.codice
+AND YEAR(attivita.data) = YEAR(CURDATE()) // YEAR(NOW())
+GROUP BY provincia.codice
+
+
+-- Versione uno, sporca, veloce e non sempre corretta
+SELECT caseificio.nome, caseficio.indirizzo, blablabla , COUNT(*) AS forme_vendute
+FROM caseificio, forma
+WHERE caseficio.codice = forma.codice_caseificio
+AND scelta = "prima"
+AND YEAR(data) = :anno_scelto
+GROUP BY caseficio.codice
+ORDER BY forme_vendute DESC
+LIMIT 0, 1
+
+-- Versione due, corretta ma più complessa da scrivere
+SELECT caseficio.nome, caseficio.indirizzo, forme_vendute
+FROM (SELECT caseificio.nome, caseficio.indirizzo,      COUNT(*) AS forme_vendute
+        FROM caseificio, forma
+        WHERE caseficio.codice = forma.codice_caseificio
+        AND scelta = "prima"
+        AND YEAR(data) = :anno_scelto 
+        GROUP BY caseficio.codice) AS tabella_A
+WHERE forme_vendute = (SELECT MAX(forme_vendute)
+                        FROM (SELECT COUNT(*) AS forme_vendute
+                        FROM caseificio, forma
+                        WHERE caseficio.codice = forma.codice_caseificio
+                        AND scelta = "prima"
+                        AND YEAR(data) = :anno_scelto 
+                        GROUP BY caseficio.codice) AS tabella_A)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
