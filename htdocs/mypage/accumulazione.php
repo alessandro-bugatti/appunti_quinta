31507d71f7c75429e315b34f1bb2a86a1ca75738
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-11-21 12:39:02 +0100

    Versione completa dell'esercizio Accumulate

--- htdocs/mypage/accumulazione.php
+++ htdocs/mypage/accumulazione.php
@@ -7,27 +7,42 @@
 <body>
 
 <?php
-    $valori = $_POST['valori'];
-    $operazione = $_POST['operazioni'];
-    echo $valori;
-    $numeri = explode(',', $valori);
-    echo '<pre>';
-    var_dump($numeri);
-    echo '</pre>';
-    for ($i = 0; $i < count($numeri); $i++){
-        echo ($numeri[$i] * 10) . '<br>';
+$valori = $_POST['valori'];
+$operazione = $_POST['operazioni'];
+
+$numeri = explode(',', $valori);
+
+for ($i = 0; $i < count($numeri); $i++) {
+    if ($operazione == 1) {
+        $numeri[$i] *= $numeri[$i];
+        }
+    if ($operazione == 2) {
+        $numeri[$i] *= $numeri[$i] * $numeri[$i];
     }
-    for($i = 0; $i < count($numeri); $i++){
-        if ($operazione == 1){
-            echo ($numeri[$i] * $numeri[$i]) . ' ';
-        }
-        if ($operazione == 2){
-            echo ($numeri[$i] * $numeri[$i] * $numeri[$i]) . ' ';
-        }
-        if ($operazione == 3){
-            echo (sqrt($numeri[$i])) . ' ';
-        }
+    if ($operazione == 3) {
+        $numeri[$i] = sqrt($numeri[$i]);
     }
+}
+$da_stampare = array_fill(0, count($numeri), false);
+if (isset($_POST['pari'])) {
+    for ($i = 0; $i < count($numeri); $i++) {
+        if ($numeri[$i] % 2 == 0)
+            $da_stampare[$i] = true;
+    }
+}
+if (isset($_POST['dispari'])) {
+    for ($i = 0; $i < count($numeri); $i++) {
+        if ($numeri[$i] % 2 != 0)
+            $da_stampare[$i] = true;
+    }
+}
+if (!isset($_POST['pari']) && !isset($_POST['dispari']))
+    echo "<p>Non è stato selezionato nessun insieme</p>";
+else
+    for ($i = 0; $i < count($numeri); $i++)
+        if ($da_stampare[$i] == true)
+            echo $numeri[$i] . " ";
+
 ?>
 
 </body>
