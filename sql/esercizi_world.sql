fc84546af462ea4b413c62ca8c1f44cde53b81b3
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2024-02-21 09:57:01 +0100

    Aggiunge l'istruzione per creare le viste

--- sql/esercizi_world.sql
+++ sql/esercizi_world.sql
@@ -8,6 +8,8 @@ SELECT COUNT(*)/(SELECT COUNT(*) FROM country)
 FROM country
 WHERE LifeExpectancy > 50
 
+-- Quante persone parlano la lingua più parlata al mondo
+
 SELECT MAX(parlanti)
 FROM (SELECT SUM(percentage*Population/100) AS parlanti
 FROM country INNER JOIN countryLanguage ON country.code = 
@@ -19,4 +21,38 @@ SELECT language, SUM(percentage*Population/100) AS parlanti
 FROM country INNER JOIN countryLanguage ON country.code = 
 countryLanguage.countryCode
 GROUP BY language
+
+SELECT MAX(Parlanti)
+FROM a
+
+-- Qual è la lingua più parlata al mondo e quante persone la parlano
+
+SELECT language, parlanti
+FROM (SELECT language, SUM(percentage*Population/100) AS parlanti
+FROM country INNER JOIN countryLanguage ON country.code = 
+countryLanguage.countryCode
+GROUP BY language) AS a
+WHERE parlanti = (SELECT MAX(parlanti) FROM 
+(SELECT SUM(percentage*Population/100) AS parlanti
+FROM country INNER JOIN countryLanguage ON country.code = 
+countryLanguage.countryCode
+GROUP BY language) AS b)
+
+CREATE TEMPORARY TABLE a
+SELECT language, SUM(percentage*Population/100) AS parlanti
+FROM country INNER JOIN countryLanguage ON country.code = 
+countryLanguage.countryCode
+GROUP BY language
+
+SELECT language, parlanti
+FROM a
+WHERE parlanti = (SELECT MAX(parlanti) FROM a)
+
+CREATE VIEW a AS
+SELECT language, SUM(percentage*Population/100) AS parlanti
+FROM country INNER JOIN countryLanguage ON country.code = 
+countryLanguage.countryCode
+GROUP BY language
+
+
 
