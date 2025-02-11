cd298c55948a5e2d0a5a1df90880fdb572041c62
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-02-11 10:22:14 +0100

    Aggiunge il file di configurazione di esempio

---
+++ slim/conf/config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'negozio';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_CHAR = 'utf8';
+
+//Sottocartella dove viene eseguita l'applicazione
+const BASE_PATH = '/slim';
+
+//Attiva il gestore di errori personalizzato
+const MY_ERROR_HANDLER = false;
