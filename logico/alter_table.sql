22fc687f6276562d8353b308e65a2d6cfaf95e30
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-11-26 12:47:22 +0100

    Completa le istruzioni DDL e inizia le DCL

--- logico/alter_table.sql
+++ logico/alter_table.sql
@@ -11,3 +11,24 @@ CHANGE COLUMN data_nascita data_pro DATE;
 
 ALTER TABLE tennista
 DROP COLUMN guadagni_totali;
+
+ALTER TABLE tennista
+ADD COLUMN id_societa int unsigned NOT NULL;
+
+CREATE TABLE societa(
+    id INT UNSIGNED AUTO_INCREMENT primary key,
+    nome varchar(50) NOT NULL
+);
+
+ALTER TABLE tennista
+ADD CONSTRAINT fk_societa
+FOREIGN KEY (id_societa)
+REFERENCES societa(id);
+
+ALTER TABLE tennista
+DROP FOREIGN KEY fk_societa;
+
+ALTER TABLE tennista
+RENAME TO giocatore;
+
+DROP TABLE giocatore;
