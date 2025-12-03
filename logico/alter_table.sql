4553e2f39ef8b43ff85029487352699d53313a06
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-12-03 21:34:53 +0100

    Aggiorna le query SQL

--- logico/alter_table.sql
+++ logico/alter_table.sql
@@ -32,3 +32,43 @@ ALTER TABLE tennista
 RENAME TO giocatore;
 
 DROP TABLE giocatore;
+
+CREATE USER manager_torneo@localhost
+IDENTIFIED BY "manager";
+
+GRANT ALL PRIVILEGES ON tennis.*
+TO manager_torneo@localhost;
+
+ALTER USER manager_torneo@localhost
+IDENTIFIED BY "torneo";
+
+DROP USER manager_torneo@localhost;
+
+-- Istruzioni DML
+
+INSERT INTO societa (id, nome) VALUES (null, "Brixia");
+
+INSERT INTO tennista (nome, cognome, data_nascita, sesso)
+VALUES ("Giorgia", "Astruni", "2002-07-10", "F");
+
+INSERT INTO tennista (nome, cognome, data_nascita, sesso)
+VALUES ("Adele", "Miscoli", "2002-07-08", "F");
+
+UPDATE tennista
+SET cognome = "Mascoli"
+WHERE ID = 2;
+
+UPDATE tennista
+SET nome = "Giorgetta",
+    cognome = "Astroni"
+WHERE ID = 1;
+
+DELETE FROM tennista
+WHERE ID = 1;
+
+SELECT *
+FROM tennista;
+
+SELECT *
+FROM tennista
+WHERE ID = 2;
