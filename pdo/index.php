7a4fa27d0cc7ffeac98157a00a56a92921e33225
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-12-06 10:18:52 +0100

    Aggiornamento con database

--- pdo/index.php
+++ pdo/index.php
@@ -5,15 +5,24 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require '../vendor/autoload.php';
+require 'conf/config.php';
 
 $app = AppFactory::create();
 
 //Istruzione super importante per il deployment
-$app->setBasePath('/esempio_rest');
+$app->setBasePath('/tennis');
 
 $app->get('/', function (Request $request,
                          Response $response,
                          array $args): Response {
-    return $response->withStatus(200);
+    $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME ;
+    $pdo = new PDO($dsn, DB_USER, DB_PASS,);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $stmt = $pdo->query ("SELECT * FROM tennista");
+    $response->getBody()->write(json_encode($stmt->fetchAll()));
+    return $response
+        ->withHeader('Content-Type', 'application/json');
+    return $response;
 });
 
+$app->run();
